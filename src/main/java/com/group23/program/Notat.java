f44593d13faf4eb2bbf32f3e30f3f01adb17e58b
Andreas Ellingsen Grønlien <andrgro@stud.ntnu.no>
2018-03-16 13:55:51 +0100

    Lagt til klasser for tabeller i db

---
+++ src/main/java/com/group23/program/Notat.java
@@ -0,0 +1,7 @@
+package com.group23.program;
+
+public class Notat extends DatabaseTable {
+    public int treningsøkt;
+    public int treningsformål;
+    public int opplevelse;
+}
---
+++ src/main/java/com/group23/program/Resultat.java
@@ -0,0 +1,10 @@
+package com.group23.program;
+
+public class Resultat {
+    public int treningsøkt;
+    public int øvelse;
+    public int kilo;
+    public int sett;
+    public int reps;
+    public String informasjon;
+}
