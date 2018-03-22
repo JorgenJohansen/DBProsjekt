71d2bbef38e7d8e825952655f66b33660af9c89a
Jørgen Buholm Johansen <jorgenbj@stud.ntnu.no>
2018-03-22 20:14:40 +0100

    Har brukt getOvelseFromOvelsesGruppe i Find

--- src/main/java/com/group23/program/Client.java
+++ src/main/java/com/group23/program/Client.java
@@ -251,6 +251,8 @@ public class Client {
     }
     private void Find(int id) {
         System.out.println("Find\n" + id);
+        ArrayList<String> results = queries.getOvelseFromOvelsesGruppe(id);
+        System.out.println(results);
     }
 
     //Sammenligner to resultater
