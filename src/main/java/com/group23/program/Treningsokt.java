855f049ff8e6a889245aa13b970f8e12db61e860
Jørgen Buholm Johansen <jorgenbj@stud.ntnu.no>
2018-03-22 17:11:12 +0100

    Fikset en til merge conflikt i klassen Treningsokt

--- src/main/java/com/group23/program/Treningsokt.java
+++ src/main/java/com/group23/program/Treningsokt.java
@@ -4,13 +4,17 @@ public class Treningsokt extends DatabaseTable{
     public String dato;
     public int varighet;
     public String informasjon;
-<<<<<<< HEAD
     public int personligForm;
     public int prestasjon;
-=======
-    public String personligForm;
-    public String prestasjon;
->>>>>>> 1b6db6e8034ac4944623ab94944ab06e01cc145e
+
+    public Treningsokt(String dato, int varighet, String informasjon, int personligForm, int presentasjon) {
+        this.dato = dato;
+        this.varighet = varighet;
+        this.informasjon = informasjon;
+        this.personligForm = personligForm;
+        this.prestasjon = prestasjon;
+
+    }
 
     public Treningsokt(int id, String dato, int varighet, String informasjon, int personligForm, int prestasjon) {
         this.id = id;
@@ -18,18 +22,9 @@ public class Treningsokt extends DatabaseTable{
         this.varighet = varighet;
         this.informasjon = informasjon;
         this.personligForm = personligForm;
-<<<<<<< HEAD
         this.prestasjon = prestasjon;
-=======
-        this.prestasjon = presentasjon;
+
     }
 
-    public Treningsokt(String dato, int varighet, String informasjon, String personligForm, String presentasjon) {
-        this.dato = dato;
-        this.varighet = varighet;
-        this.informasjon = informasjon;
-        this.personligForm = personligForm;
-        this.prestasjon = presentasjon;
->>>>>>> 1b6db6e8034ac4944623ab94944ab06e01cc145e
-    }
+    
 }
